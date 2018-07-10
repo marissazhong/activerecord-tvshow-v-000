5beabb41a164d758569a14517a83ee99946c245f
Marissa Zhong <mwz2101@columbia.edu>
2018-07-10 12:12:39 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,7 +2,7 @@ class Show < ActiveRecord::Base
   def self.highest_rating
     Show.maximum(:rating)
   end
-  def most_popular_show
+  def self.most_popular_show
     Show.highest_rating
   end
 end
